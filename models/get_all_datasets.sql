2a5f3b1a9f050ee952d1e722c21c215158a5f3c9
krasmm <krasimir.bambalov@mightyhive.com>
2022-10-25 14:21:34 +0200

    "get_all_datasets finalized"

--- models/get_all_datasets.sql
+++ models/get_all_datasets.sql
@@ -1,16 +1,4 @@
-{% set datasets = [] %}
-{% set get_all_datasets %}
     SELECT
         schema_name AS dataset_id
     FROM
-        {{var(project_id)}}.{{var(dataset_location)}}.INFORMATION_SCHEMA.SCHEMATA
-{% endset %}
-{% do log("Querying schemata." , info=True) %}
-{% set results = run_query(get_all_datasets)%}
-
-{% do log("Getting datasets." , info=True) %}
-{% if execute %}
-{% set datasets = results.columns[0].values() %}
-{% else %}
-{% set datasets = [] %}
-{% endif %}
+        {{var("project_id")}}.{{var("dataset_location")}}.INFORMATION_SCHEMA.SCHEMATA
