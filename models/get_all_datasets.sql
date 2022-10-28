773cad55e757bd6e34778462a6e670ab67f68258
krasmm <krasimir.bambalov@mightyhive.com>
2022-10-28 09:22:01 +0200

    "model changed"

--- models/get_all_datasets.sql
+++ models/get_all_datasets.sql
@@ -1,16 +1,4 @@
-{% set datasets = [] %}
-{% set results = [] %}
-
-{% set get_all_datasets %}   
-    SELECT
-        schema_name AS dataset_id
-    FROM
-        {{var("project_id")}}.{{var("dataset_location")}}.INFORMATION_SCHEMA.SCHEMATA
-{% endset %}
-{% do log("Querying schemata." , info=True) %}
---{% set results = run_query(get_all_datasets)%}
---{% set datasets = results.columns[0].values() %}
-
-{# {% for dataset in datasets %}
-SELECT * FROM {{var("project_id")}}.{{var("dataset")}}.__TABLES__ 
-{% endfor %} #}
+SELECT
+    schema_name AS dataset_id
+FROM
+    {{var("project_id")}}.{{var("dataset_location")}}.INFORMATION_SCHEMA.SCHEMATA
